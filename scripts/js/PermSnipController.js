84ff38ce4ae93736d6c580c3f32d84f915636de0
mhgbrown <brown.mhg+git@gmail.com>
2016-10-29 16:32:21 +0200

    Set website height
    * Stagger movement of perm snips

--- scripts/js/PermSnipController.js
+++ scripts/js/PermSnipController.js
@@ -2,28 +2,39 @@ import $ from 'jquery';
 
 export default class PermSnipController {
   constructor($snips) {
+    var self = this;
+
     this.$snips = $snips;
 
     this.move = this.move.bind(this);
 
-    this.move();
+    this.$snips.each(function(index, el) {
+      self.move($(el), false);
+
+      // lol end me
+      window.setTimeout(function() {
+        self.move($(el));
+      }, index * 1000);
+    });
   }
 
-  move() {
-    this.$snips.each(function() {
-      var $this = $(this);
-      var top = Math.random() * ($(document).height() - $this.height());
-      var left = Math.random() * ($(document).width() - $this.width());
+  move($element, reschedule=true) {
+    var self = this;
+    var top = Math.random() * ($(document).height() - $element.height());
+    var left = Math.random() * ($(document).width() - $element.width());
 
-      $this.animate({
-        top,
-        left
-      }, {
-        duration: 0,
-        easing: 'linear'
-      });
+    $element.css({
+      top,
+      left,
+      'visibility': 'visible'
     });
 
-    window.setTimeout(this.move, 5000);
+    if(!reschedule) {
+      return;
+    }
+
+    window.setTimeout(function() {
+      self.move($element);
+    }, 5000);
   }
 }
