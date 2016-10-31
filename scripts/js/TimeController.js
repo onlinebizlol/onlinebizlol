351d71a02737724486203505ff847f11a80c1f0f
mhgbrown <brown.mhg+git@gmail.com>
2016-10-31 18:21:07 +0100

    Add support for DST
    * lol

--- scripts/js/TimeController.js
+++ scripts/js/TimeController.js
@@ -7,6 +7,18 @@ export default class TimeController {
     this.tick()
   }
 
+  // http://stackoverflow.com/questions/11887934/check-if-daylight-saving-time-is-in-effect-and-if-it-is-for-how-many-hours
+  stdTimezoneOffset(date) {
+    var jan = new Date(date.getFullYear(), 0, 1);
+    var jul = new Date(date.getFullYear(), 6, 1);
+    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+  }
+
+  // http://stackoverflow.com/questions/11887934/check-if-daylight-saving-time-is-in-effect-and-if-it-is-for-how-many-hours
+  isDST(date) {
+    return date.getTimezoneOffset() < this.stdTimezoneOffset(date);
+  }
+
   dateToUTC(date) {
     return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
       date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
@@ -22,7 +34,8 @@ export default class TimeController {
   // http://codegolf.stackexchange.com/questions/42289/return-the-clock-emoji-closest-to-the-current-time-when-run#answer-42306
   tick() {
     var now = this.dateToUTC(new Date());
-    now.setHours(now.getHours() + 2);
+    var offset = this.isDST(now) ? 2 : 1;
+    now.setHours(now.getHours() + offset);
 
     var dateString = `${this.pad(now.getHours(), 2)}:${this.pad(now.getMinutes(), 2)}:${this.pad(now.getSeconds(), 2)}`;
     var emojiCode = ~~(now.getHours() % 12 * 2 + now.getMinutes() / 30 + .5);
